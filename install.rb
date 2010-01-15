2541f2b1a4bf15ab817e0e05fe00e2d11d3528c0
alain <alainbloch@gmail.com>
2010-01-15 13:02:03 -0800

    added installation code.

--- install.rb
+++ install.rb
@@ -1,1 +1,14 @@
-# Install hook code here
+require 'fileutils'
+
+here = File.dirname(__FILE__)
+there = defined?(RAILS_ROOT) ? RAILS_ROOT : "#{here}/../../.."
+
+FileUtils.mkdir_p("#{there}/public/javascripts/uploadify/")
+
+puts "Installing Uploadify..."
+FileUtils.cp_r("#{here}/public/javascripts/uploadify/", "#{there}/public/javascripts/uploadify/")
+FileUtils.cp("#{here}/public/images/cancel.png", "#{there}/public/images/")
+
+puts "================================Installation Complete!==========================================="
+
+puts IO.read(File.join(File.dirname(__FILE__), '../README.rdoc'))
