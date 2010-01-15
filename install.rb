878f919cc4ee957eed0384556c97f7537d2c2189
alain <alainbloch@gmail.com>
2010-01-15 13:43:25 -0800

    revised install rb to add in some installation instructions.

--- install.rb
+++ install.rb
@@ -9,4 +9,8 @@ FileUtils.cp("#{here}/public/images/cancel.png", "#{there}/public/images/")
 
 puts "================================Installation Complete!==========================================="
 
-puts IO.read(File.join(File.dirname(__FILE__), '../README.rdoc'))
+puts "You will need to edit config/initializers/session_store.rb to add in the new Middleware."
+
+puts "You will also need to have the jquery and jquery-ui libraries installed."
+
+puts "Refer to the README.rdoc for specific instructions."
